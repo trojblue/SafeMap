1885d581fcddb05b64bafb317eb9c6571e57b964
Somiona <somionat@gmail.com>
2021-01-18 04:50:07 +0800

    added dcument for api username

--- combined/routes/apiRoutes.js
+++ combined/routes/apiRoutes.js
@@ -32,6 +32,57 @@ router.get("/testing", function (req, res) {
 });
 
 const { getUsername } = require("../controller/userInfoController");
+/**
+ * @swagger
+ * tags:
+ *   name: UserInfo
+ *   description: API for Get User Info
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     cookieAuth:
+ *       type: apiKey
+ *       in: cookie
+ *       name: Token
+ *     cookieName:
+ *       type: apiKey
+ *       in: cookie
+ *       name: User
+ */
+
+/**
+ * @swagger
+ * /username:
+ *   get:
+ *     description: Get the user name of current signed in user
+ *     tags:
+ *       - UserInfo
+ *     security:
+ *       - cookieAuth: []
+ *       - cookieName: []
+ *     responses:
+ *       "200":
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  code:
+ *                    type: number
+ *                    oneOf:
+ *                      - 0: Success
+ *                      - 100: Not Logged In
+ *                      - 101: Server Internal Error
+ *                  data:
+ *                    type: string
+ *                    oneOf:
+ *                      - User's email: if success
+ *                      - error info: if request failed
+ *                    example: "abc@abc.abc"
+ */
 router.get("/username", getUsername);
 
 //! this two lines works after firebase is properly configured
